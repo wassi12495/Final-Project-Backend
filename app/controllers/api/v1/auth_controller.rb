6a915c1cc406731cc7f025667a3b13accdc47f40
Josh Wasserman <github email address>
2018-01-31 14:00:32 -0500

    Fixed AuthController. Added Auth routes.

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::AuthControll < ApplicationController
+class Api::V1::AuthController < ApplicationController
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
       resources :users
       resources :workouts
       resources :exercises
-      
+      resources :auth
+
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
