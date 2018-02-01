367880f166ca9da0fd36ecbf436aabe15eb2d244
Josh Wasserman <github email address>
2018-02-01 14:10:32 -0500

    Finished backend Auth with JWT and Figaro

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,20 +5,30 @@ class Api::V1::AuthController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
+
       render json: {
         id: @user.id,
         username: @user.username,
+        first_name: @user.first_name,
+        last_name: @user.last_name,
+        workouts: @user.workouts,
         jwt: JWT.encode({user_id: @user.id}, ENV['secret_key'], 'HS256')
       }
     else
+
       render json: {error: 'User not found'}, status: 404
     end
   end
 
-
-
-
-  def index
+  def show
+    if current_user
+      render json: {
+        id: current_user.id,
+        username: current_user.username
+      }
+    else
+      render json: {error: 'No id present on headers'}, status: 404
+    end
   end
 
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+
+  def current_user
+    @user ||= User.find_by(id: user_id)
+  end
+
+  def user_id
+    decoded_token.first['user_id']
+  end
+
+  def decoded_token
+    begin
+       JWT.decode(request.headers['Authorization'], ENV['secret_key'])
+     rescue JWT::DecodeError
+      [{}]
+     end
+  end
 end
