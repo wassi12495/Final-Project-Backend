28e2ddbafebf491d2bcf9979dba05d107d5cf8ae
Josh Wasserman <github email address>
2018-02-08 10:31:12 -0500

    Working on workouts#create. => Converting current workout to workout.

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -10,7 +10,24 @@ class Api::V1::WorkoutsController < ApplicationController
   end
 
   def create
-    render json: {message: "You are still in testing mode!"}
+    if current_user
+      routine_id = params["routine_id"]
+      current_workout_id = params["currentWorkout_id"]
+      exercises = params["exercises"]
+      byebug
+      @workout = Workout.new(user_id: current_user["id"], routine_id: routine_id, time_of_workout:Time.now)
+      render json: {message: "STILL IN TESTING"}
+      # if @workout.save
+      #
+      #
+      #
+      # else
+      #
+      #   render json: {error: "Failed to create workout."}, status: 401
+      # end
+    else
+      render json: {message: "Failed to authenticate user."}, status: 404
+    end
   end
 
 
