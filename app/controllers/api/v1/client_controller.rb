368a74b44d7d6717779ced6f4196c8d21cedc9b9
Josh Wasserman <github email address>
2018-02-13 18:29:11 -0500

    Fixed authentication bug in client#accept_request.Done.

--- app/controllers/api/v1/client_controller.rb
+++ app/controllers/api/v1/client_controller.rb
@@ -3,10 +3,21 @@ class Api::V1::ClientController < ApplicationController
 
   def accept_request
     if current_user
+      trainer = User.find_by(username: params[:request][:trainer][:username])
+      user = User.find(current_user[:id])
+      user.update(trainer_id: trainer[:id])
 
-      byebug
+      if user.authenticate(params[:password])
+        user.update(password: params[:password], password_confirmation: params[:password])
 
-      render json: {message: ["Still in development"]}
+        req = AddClientRequest.find_by(client_id: user[:id])
+        req.destroy
+        render json: {message: "#{trainer[:username]} is now your trainer!"}
+
+      else
+        byebug
+        render json: {message: ["Still in development"]}
+      end
 
     else
 
