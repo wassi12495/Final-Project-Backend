0d49c59092703175fb5c2f9c4a69a9bca80c6186
Josh Wasserman <github email address>
2018-03-05 22:49:04 -0500

    iss#4.1: Change exercises table to include a columns array.

--- db/migrate/20180204031334_create_exercises.rb
+++ db/migrate/20180204031334_create_exercises.rb
@@ -5,6 +5,7 @@ class CreateExercises < ActiveRecord::Migration[5.1]
       t.string :description
       t.belongs_to :exercise_category
       t.belongs_to :user, optional:true, foreign_key: true
+      t.string :columns, default: [], array: true
 
       t.timestamps
     end
