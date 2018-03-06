57089469a32809ab00d694d6f0954b0046d95473
Josh Wasserman <github email address>
2018-03-05 23:17:36 -0500

    iss#4.2: Changed exercise_category to string in exercises table.

--- db/migrate/20180204031334_create_exercises.rb
+++ db/migrate/20180204031334_create_exercises.rb
@@ -3,7 +3,7 @@ class CreateExercises < ActiveRecord::Migration[5.1]
     create_table :exercises do |t|
       t.string :name
       t.string :description
-      t.belongs_to :exercise_category
+      t.string :exercise_category
       t.belongs_to :user, optional:true, foreign_key: true
       t.string :columns, default: [], array: true
 
