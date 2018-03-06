a4e09291c64bd98c84c276ae0533ca525f46999a
Josh Wasserman <github email address>
2018-03-05 23:27:33 -0500

    iss#4.2: Modified Exercise Serializer, added :columns.

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -1,5 +1,5 @@
 class ExerciseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :exercise_category
+  attributes :id, :name, :description, :exercise_category, :columns
 
   # belongs_to :workout
 
